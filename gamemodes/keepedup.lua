a7471935aa019bbf353e8b89144c091eb180c84a
Henry <henryquoctran@gmail.com>
2014-04-30 15:36:47 -0700

    KeepEdUp app

---
+++ gamemodes/keepedup.lua
@@ -0,0 +1,37 @@
+
+local BaseGM = reload("gamemodes.basegm")
+local GameMode = class("KeepEdUpMode", BaseGM)
+
+function GameMode:initialize()
+    self.super:initialize()
+
+    local player = entityfactory:create("ed")
+    player:setPos(50, 50)
+    player:spawn()
+
+    local player2 = entityfactory:create("ed")
+    player2:setPos(50, 50)
+    player2:spawn()
+
+    for i = 1, 15 do
+        local ent = entityfactory:create("physentity")
+        ent:setPos(0+20*i, 20)
+        ent:spawn()
+
+        ent:setVelocity(100, 0)
+        ent:setTorque(math.random(-1, 1))
+    end
+
+    local tilemap = entityfactory:create("tilemap")
+    tilemap:setPos(300, 400)
+    tilemap:spawn()
+end
+
+function GameMode:update(dt)
+    world:updateEntities(dt)
+end
+
+function GameMode:postDraw()
+end
+
+return GameMode
